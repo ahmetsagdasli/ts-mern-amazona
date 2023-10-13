294e609becae4fd4d141d4c878c4fb123da31358
ahmetsagdasli <ahmetssagdasli@gmail.com>
2023-10-13 15:47:17 +0300

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <header> TS AMAZONA</header>
+      <main></main>
+      <footer>
+        All rights are reserved.
+      </footer>
+    </div>
+  )
+}
+
+export default App

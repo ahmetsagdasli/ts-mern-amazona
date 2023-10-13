cab6aa6a8ebd486ea44852daca741a748611131b
ahmetsagdasli <ahmetssagdasli@gmail.com>
2023-10-14 00:34:12 +0300

    boostrap-adding

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,47 @@
-import React from 'react'; // Import React (assuming you're using React)
-import reactLogo from './assets/react.svg';
-import viteLogo from './vite.svg'; // Correct the path to viteLogo
-import './App.css';
-import { sampleProducts } from './data';
+import React from 'react'
+import { sampleProducts } from './data'
+import { Container, Nav, Navbar, Row, Col } from 'react-bootstrap'
 
 function App() {
   return (
-    <div>
-      <header>TS AMAZONA</header>
+    <div className="d-flex flex-column vh-100">
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand>tsamazona</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href="/cart" className="nav-link">
+              Cart
+            </a>
+            <a href="/signin" className="nav-link">
+              Sign In
+            </a>
+          </Nav>
+        </Navbar>
+      </header>
       <main>
-        <ul>
-          {sampleProducts.map((product) => ( 
-            <li key={product.slug}> {}
-              <img src={product.image} alt={product.name} className='product-image' /> {}
-              <h2>{product.name}</h2>
-              <p>${product.price}</p>
-            </li>
-          ))}
-        </ul>
+        <Container className="mt-3">
+          <Row>
+            {sampleProducts.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3}>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="product-image"
+                />
+                <h2>{product.name}</h2>
+                <p>${product.price}</p>
+              </Col>
+            ))}
+          </Row>
+        </Container>
       </main>
-      <footer>All rights reserved.</footer>
+      <footer>
+        <div className="text-center">All rights reserved.</div>
+      </footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
